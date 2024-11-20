64ab844f9c065b43a99bed05fdd765aaaf7cf256
Fredo <160517774+elfredoo@users.noreply.github.com>
2024-11-20 19:48:34 +0100

    First commit

---
+++ src/main/java/com/website/securitykonfiguracja/SecurityKonfiguracjaApplication.java
@@ -0,0 +1,13 @@
+package com.website.securitykonfiguracja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityKonfiguracjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityKonfiguracjaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/website/securitykonfiguracja/SecurityKonfiguracjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.securitykonfiguracja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityKonfiguracjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
